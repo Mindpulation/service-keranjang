2ef38349f725402234701f7edb67177c29b14b61
root <root@localhost.localdomain>
2020-08-12 17:43:15 +0000

    Update address DB

--- controller/index.js
+++ controller/index.js
@@ -3,7 +3,7 @@ const Model = require('../model/Cart/index');
 class Controller{
 
   constructor(){
-    this.model = new Model("mongodb://indo1:2ad68fdaa8a4207a35ec22ba7beb34cd2425b247@mongo-indo1:27017");
+    this.model = new Model("mongodb://indo1:2ad68fdaa8a4207a35ec22ba7beb34cd2425b247@mongo-indo1:27017/KeranjangDB");
   }
 
 }
